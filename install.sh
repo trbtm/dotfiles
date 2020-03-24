399196496f45a5411252dbeaa39583fe1d7bde91
Thore Bartholomäus <thore.bar@gmail.com>
2020-03-24 16:30:10 +0100

    - added gitlab TU Berlin - Refactored install.sh

--- install.sh
+++ install.sh
@@ -9,32 +9,26 @@ echo "Initialized git submodules."
 #
 # zshrc
 #
-cd $HOME
-rm -rf ./.zshrc > /dev/null
-ln -s ./.dotfiles/.zshrc ./.zshrc
-cd $HOME/.dotfiles
+
+rm -rf $HOME/.zshrc > /dev/null
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 echo "Linked zshrc."
 
 #
 # powerlevel10k theme
 #
-cd ./ohmyzsh/themes
-rm -rf ./powerlevel10k.zsh-theme > /dev/null
-ln -s ../../powerlevel10k/powerlevel10k.zsh-theme ./powerlevel10k.zsh-theme
-cd $HOME
-rm -rf ./.p10k.zsh > /dev/null
-ln -s ./.dotfiles/.p10k.zsh ./.p10k.zsh
-cd $HOME/.dotfiles
+rm -rf $HOME/.dotfiles/ohmyzsh/themes/powerlevel10k.zsh-theme > /dev/null
+ln -s $HOME/.dotfiles/powerlevel10k/powerlevel10k.zsh-theme $HOME/.dotfiles/ohmyzsh/themes/powerlevel10k.zsh-theme
+rm -rf $HOME/.p10k.zsh > /dev/null
+ln -s $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
 
 echo "Linked powerlevel10k theme."
 
 #
 # zsh-autosuggestions
 #
-cd ./ohmyzsh/plugins
-rm -rf ./zsh-autosuggestions > /dev/null
-ln -s ../../zsh-autosuggestions ./zsh-autosuggestions
-cd $HOME/.dotfiles
+rm -rf $HOME/.dotfiles/ohmyzsh/plugins/zsh-autosuggestions > /dev/null
+ln -s $HOME/.dotfiles/zsh-autosuggestions $HOME/.dotfiles/ohmyzsh/plugins/zsh-autosuggestions
 echo "Linked zsh-autosuggestions."
 
 #
@@ -42,10 +36,8 @@ echo "Linked zsh-autosuggestions."
 #
 link_gitconfig()
 {
-  cd $HOME
-  rm -rf ./.gitconfig > /dev/null
-  ln -s ./.dotfiles/.gitconfig ./.gitconfig
-  cd $HOME/.dotfiles
+  rm -rf $HOME/.gitconfig > /dev/null
+  ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
   echo "Linked gitconfig."
 }
 
@@ -63,10 +55,8 @@ done
 #
 link_ssh_config()
 {
-  cd $HOME
-  rm -rf ./.ssh/config > /dev/null
-  ln -s ./.dotfiles/ssh_config ./.ssh/config
-  cd $HOME/.dotfiles
+  rm -rf $HOME/.ssh/config > /dev/null
+  ln -s $HOME/.dotfiles/ssh_config $HOME/.ssh/config
   echo "Linked ssh/config."
 }
 

174a1f7bd61c5ef305e47fd2a662084186d8bd83
Thore Bartholomäus <thore@Thores-MacBook-Pro.local>
2020-05-31 16:41:54 +0200

    fish config for pyenv

--- config.fish
+++ config.fish
@@ -1,2 +1,11 @@
-starship init fish | source
+if type -q starship
+    starship init fish | source
+end
+
+if type -q pyenv
+    set -Ux PYENV_ROOT $HOME/.pyenv
+    set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+    pyenv init - | source
+end
+
 source ~/.dotfiles/aliases.fish

f32ef350a4546cefe053e0e9010c495e693cf7a4
Thore Bartholomäus <thore.bar@gmail.com>
2020-10-14 23:35:50 +0200

    - Moved local settings and aliases to custom folder

--- shell/universal_settings.sh
+++ shell/universal_settings.sh
@@ -8,15 +8,3 @@ export VISUAL='nano'
 
 # custom aliases, configs, etc.
 source ~/.dotfiles/shell/custom/index.sh
-
-# rust
-export PATH=$HOME/.cargo/bin:$PATH
-
-# pyenv
-if [ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
-    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-fi
-
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
-fi

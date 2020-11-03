414c76757f3b3a6cb77ff93abb94712f009a166e
Thore Bartholomäus <thore.bar@gmail.com>
2020-11-03 23:21:18 +0100

    - removed setup scripts

--- setup/insomnia.sh
+++
@@ -1,5 +0,0 @@
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee /etc/apt/sources.list.d/insomnia.list
-sudo rm /etc/apt/trusted.gpg.d/home:manuelschneid3r.asc
-wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y insomnia

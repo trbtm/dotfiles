bf67da3f979c0385abaa1316cc6167c11027f0dc
Thore Bartholomäus <thore.bar@gmail.com>
2022-04-05 11:27:42 +0200

    Some changes

--- shell/custom/custom_macbook.sh
+++ shell/custom/custom_macbook.sh
@@ -1,20 +1,55 @@
- # python
-alias python3.10="/opt/homebrew/Cellar/python@3.10/3.10.1/bin/python3"
-alias python3.9="/opt/homebrew/Cellar/python@3.9/3.9.10/bin/python3"
-
-# brew
-alias x86_brew="/usr/local/bin/brew"
+#
+# nvm
+#
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-# Exercism
-export PATH=$HOME/Exercism/bin:$PATH
+autoload -U add-zsh-hook
+load-nvmrc() {
+    local node_version="$(nvm version)"
+    local nvmrc_path="$(nvm_find_nvmrc)"
 
-# rust
-export PATH=$HOME/.cargo/bin:$PATH
+    if [ -n "$nvmrc_path" ]; then
+        local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
 
-# nvm
+        if [ "$nvmrc_node_version" = "N/A" ]; then
+            nvm install
+        elif [ "$nvmrc_node_version" != "$node_version" ]; then
+            nvm use
+        fi
+    elif [ "$node_version" != "$(nvm version default)" ]; then
+        echo "Reverting to nvm default version"
+        nvm use default
+    fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+#
+# pyenv
+#
+
+eval "$(pyenv init --path)"
+
+#
+# Exercism
+#
+
+export PATH=$HOME/Exercism/bin:$PATH
+
+#
+# rust
+#
+
+export PATH=$HOME/.cargo/bin:$PATH
+
+#
+# misc
+#
+
+alias fleetlabui_git_daemon='git daemon --base-path=$(realpath ~)/Fleetlab --export-all --enable=receive-pack --reuseaddr --informative-errors --verbose --port=1000'
